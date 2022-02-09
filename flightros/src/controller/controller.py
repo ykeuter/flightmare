65b64036fb8c26bb59fc80bc17cf8b87e5259e00
Yvo Keuter <ypmkeuter@gmail.com>
2022-02-09 20:44:42 +0100

    add log info

--- flightros/src/controller/controller.py
+++ flightros/src/controller/controller.py
@@ -13,6 +13,23 @@ class Controller:
         rospy.spin()
 
     def img_callback(self, msg):
+        rospy.loginfo(
+            (
+                "height ({}): {}\n" +
+                "width ({}): {}\n" +
+                "encoding ({}): {}\n" +
+                "is_bigendian ({}): {}\n" +
+                "step ({}): {}\n" +
+                "data ({}): {}\n"
+            ).format(
+                type(msg.height), msg.height,
+                type(msg.width), msg.width,
+                type(msg.encoding), msg.encoding,
+                type(msg.is_bigendian), msg.is_bigendian,
+                type(msg.step), msg.step,
+                type(msg.data), max(msg.data)
+            )
+        )
         self._pub.publish(Cmd(0, [3, 3, 3, 3]))
 
 if __name__ == '__main__':

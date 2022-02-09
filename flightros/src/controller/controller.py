0768581ec779e229af664b0c67c6db9a8a11bdfa
Yvo Keuter <ypmkeuter@gmail.com>
2022-02-09 15:52:52 +0100

    use controller

--- flightros/src/controller/controller.py
+++ flightros/src/controller/controller.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import String
+from flightros.msg import Cmd
+from sensor_msgs.msg import Image
 
 class Controller:
     def __init__(self):
         self._pub = None
         
     def run(self):
-        self._pub = rospy.Publisher('cmd', String, queue_size=1)
-        rospy.Subscriber("rgb", String, self.img_callback)
+        self._pub = rospy.Publisher('cmd', Cmd, queue_size=1)
+        rospy.Subscriber("rgb", Image, self.img_callback)
         rospy.spin()
 
     def img_callback(self, msg):
-        self._pub.publish("test")
+        self._pub.publish(Cmd(0, [3, 3, 3, 3]))
 
 if __name__ == '__main__':
     rospy.init_node('controller', anonymous=True)

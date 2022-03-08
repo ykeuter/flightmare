6d4df7947e05a8665a3091f3e79c1ef161db9f91
Yvo Keuter <ypmkeuter@gmail.com>
2022-03-08 15:41:42 +0100

    quat to euler function

--- flightros/src/trainer/trainer.py
+++ flightros/src/trainer/trainer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import rospy
+import math
 
 class Trainer:
     EPOCH_T = 5
@@ -33,6 +34,23 @@ class Trainer:
         # update reward
         # if T > EPOCh_T
         # reset
+
+    def quaternion_to_euler(self, x, y, z, w):
+        t0 = +2.0 * (w * x + y * z)
+        t1 = +1.0 - 2.0 * (x * x + y * y)
+        roll = math.atan2(t0, t1)
+
+        t2 = +2.0 * (w * y - z * x)
+        t2 = +1.0 if t2 > +1.0 else t2
+        t2 = -1.0 if t2 < -1.0 else t2
+        pitch = math.asin(t2)
+
+        t3 = +2.0 * (w * z + x * y)
+        t4 = +1.0 - 2.0 * (y * y + z * z)
+        yaw = math.atan2(t3, t4)
+
+        return roll, pitch, yaw
+
 
 if __name__ == '__main__':
     rospy.init_node('trainer', anonymous=True)

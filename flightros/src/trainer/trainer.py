77894988c2a04c2e0791ddf2658500c06708b7ac
Yvo Keuter <ypmkeuter@gmail.com>
2022-03-15 16:00:54 +0100

    loop over genomes

--- flightros/src/trainer/trainer.py
+++ flightros/src/trainer/trainer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import rospy
 from flightros.msg import State
-from flightros.srv import ResetSim, ResetSimRequest
+from flightros.srv import ResetSim, ResetSimRequest, ResetCtl, ResetCtlRequest
 from geometry_msgs.msg import Pose, Point, Quaternion, Vector3, Twist
 import os
 import neat
@@ -11,8 +11,10 @@ class Trainer:
 
     def __init__(self):
         self._reset_sim = None
+        self._reset_ctl = None
         self._is_resetting = False
         self._population = None
+        self._generator = None
         self._current_reward = 0
         self._current_agent = None
         self._prev_t = 0
@@ -27,33 +29,32 @@ class Trainer:
             config_path,
         )
         self._population = neat.Population(config)
+        self._generator = iter(self._population)
         rospy.wait_for_service('reset_sim')
         self._reset_sim = rospy.ServiceProxy('reset_sim', ResetSim)
+        self._reset_ctl = rospy.ServiceProxy('reset_ctl', ResetCtl)
         rospy.Subscriber("state", State, self.state_callback)
         rospy.spin()
 
     def _reset(self):
-        self._is_resetting = True
-        # set reward
-        self._current_agent = self._get_next_agent()
+        self._current_agent.fitness = self._current_reward
+        self._current_agent = next(self._generator)
         self._current_reward = 0
         # reset controller
         try:
-            self._reset_sim(self._gen_new_state())
+            self._reset_ctl(self._get_weights(self._current_agent))
+            self._reset_sim(self._get_random_state())
         except rospy.ServiceException as e:
             print("Service call failed: %s"%e)
 
-    def _gen_new_state(self):
+    def _get_random_state(self):
         return ResetSimRequest(
             Pose(Point(0, 0, 7), Quaternion(1, 0, 0, 0)),
             Twist(Vector3(0, 0, 0), Vector3(0, 0, 0))
         )
 
-    def _get_next_agent(self):
-        pass
-
-    def _next_generation(self):
-        pass
+    def _get_weights(self, genome):
+        return ResetCtlRequest(data=[1, 2, 3])
 
     def state_callback(self, msg):
         t = msg.time.to_sec()
@@ -64,6 +65,7 @@ class Trainer:
         # update reward
         rospy.loginfo("\ntime: {:.2f}\n".format(msg.time.to_sec()))
         if t > self.EPOCH_LENGTH:
+            self._is_resetting = True
             self._reset()
 
 

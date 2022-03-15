3cecf6e7c0560048ec4a60840e130e38c5cb3a95
Yvo Keuter <ypmkeuter@gmail.com>
2022-03-15 16:30:30 +0100

    add mlp class

---
+++ flightros/src/trainer/mlp.py
@@ -0,0 +1,18 @@
+class Mlp:
+    def __init__(self):
+        self.hidden_layers_weights = []
+        self.output_layer_weights = None
+
+    def activate(self):
+        pass
+
+    def to_msg(self):
+        pass
+
+    @staticmethod
+    def from_msg():
+        pass
+
+    @staticmethod
+    def from_cppn():
+        pass
